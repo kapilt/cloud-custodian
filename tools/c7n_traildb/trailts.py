6c0687003a1c8e771969918033c458971a8c1e32
Kapil Thangavelu <kapil.thangavelu@capitalone.com>
2017-03-03 12:33:56 -0500

    various improvements

--- tools/c7n_traildb/trailts.py
+++ tools/c7n_traildb/trailts.py
@@ -26,7 +26,7 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s: %(name)s:%(levelname)s %(message)s")
 
-#  logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
+# logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
 logging.getLogger('requests').setLevel(logging.INFO)
 logging.getLogger('s3transfer').setLevel(logging.INFO)
 logging.getLogger('botocore.vendored').setLevel(logging.WARNING)
@@ -51,15 +51,13 @@ CONFIG_SCHEMA = {
         'accounts': {
             'type': 'array',
             'items': {
-                'type': {
-                    'type': 'object',
-                    'required': ['name', 'bucket', 'regions', 'title'],
-                    'properties': {
-                        'name': {'type': 'string'},
-                        'title': {'type': 'string'},
-                        'bucket': {'type': 'string'},
-                        'regions': {'type': 'array', 'items': {'type': 'string'}}
-                        }
+                'type': 'object',
+                'required': ['name', 'bucket', 'regions', 'title'],
+                'properties': {
+                    'name': {'type': 'string'},
+                    'title': {'type': 'string'},
+                    'bucket': {'type': 'string'},
+                    'regions': {'type': 'array', 'items': {'type': 'string'}}
                 }
             }
         }
@@ -72,17 +70,18 @@ def process_traildb(db, influx, account_name, region, since=None):
     t = md.tables['events']
 
     qt = time.time()
-    log.info("query account:%s region:%s services time:%0.2f",
-             account_name, region, time.time()-qt)
+    log.debug("query account:%s region:%s services time:%0.2f incremental:%s",
+              account_name, region, time.time()-qt, since)
 
     record_count = 0
     for b in ['console', 'program']:
         for f in ['user_id', 'event_name', 'user_agent', 'error_code']:
             if b == 'console' and f == 'user_agent':
                 continue
-            q = query_by(t, f, b, since)
+            q = query_by(t, f, b, since=since)
             qt = time.time()
             results = q.execute().fetchall()
+
             log.debug(
                 "query account:%s region:%s bucket:%s field:%s points:%d time:%0.2f",
                 account_name, region, b, f, len(results), time.time()-qt)
@@ -162,7 +161,7 @@ def query_by(
 
     if since:
         query = query.where(
-            rdb.text("short_time > %s" % (since.strftime("%Y-%m-%dT%H:%M"))))
+            rdb.text("short_time > '%s'" % (since.strftime("%Y-%m-%dT%H:%M"))))
 
     return query
 
@@ -179,8 +178,8 @@ def index_account(config, region, account, day, incremental):
     name = account.get('name')
     key_template = config.get('key_template')
 
-    log.info("processing account:%s region:%s day:%s",
-             name, region, day.strftime("%Y/%m/%d"))
+    log.debug("processing account:%s region:%s day:%s",
+              name, region, day.strftime("%Y/%m/%d"))
 
     with tempfile.NamedTemporaryFile(suffix='.db.bz2', delete=False) as fh:
         key_data = dict(account)
@@ -207,37 +206,42 @@ def index_account(config, region, account, day, incremental):
 
         t = time.time()
         since = incremental and day or None
+
         record_count = process_traildb(
             rdb.create_engine("sqlite:////%s" % fh.name[:-4]),
             influx, name, region, since)
         log.debug("indexed %s in %0.2f",  fh.name, time.time()-t)
         os.remove(fh.name[:-4])
-        log.info("account:%s day:%s region:%s records:%d complete:%0.2f",
-                 name, region, day.strftime("%Y-%m-%d"),
-                 record_count,
-                 time.time()-st)
+        log.debug("account:%s day:%s region:%s records:%d complete:%0.2f",
+                  name, day.strftime("%Y-%m-%d"), region,
+                  record_count,
+                  time.time()-st)
+
+    return {'time': time.time()-st, 'records': record_count, 'region': region,
+            'account':  name, 'day': day.strftime("%Y-%m-%d")}
 
 
 def get_date_range(start, end):
-    if start:
+    if start and not isinstance(start, datetime.datetime):
         start = parse_date(start)
-    if end:
+    if end and not isinstance(end, datetime.datetime):
         end = parse_date(end)
 
+    now = datetime.datetime.utcnow().replace(
+        hour=0, minute=0, second=0, microsecond=0)
     if end and not start:
         raise ValueError("Missing start date")
     elif start and not end:
-        end = datetime.datetime.utcnow()
+        end = now
     if not end and not start:
-        return [
-            datetime.datetime.utcnow() - datetime.timedelta(seconds=60 * 60)]
+        return [now - datetime.timedelta(seconds=60 * 60)]
 
     days = []
-    for n in range(1, (end-start).days):
-        days.append(start + datetime.timedelta(n))
+    n_start = start.replace(hour=0, minute=0, second=0, microsecond=0)
+    for n in range(1, (end-n_start).days):
+        days.append(n_start + datetime.timedelta(n))
     days.insert(0, start)
-
-    if start != end:
+    if n_start != end:
         days.append(end)
 
     return days
@@ -260,20 +264,46 @@ def get_incremental_starts(config, default_start):
                 order by time desc limit 1""" % (
                 account['name'], region))
             if res is None or len(res) == 0:
-                account_starts[account['name']] = default_start
-            account_starts[account['name']] = parse_date(
-                res.raw['series'][0]['values'][0][0])
+                account_starts[(account['name'], region)] = default_start
+                continue
+            # its all utc
+            account_starts[(account['name'], region)] = parse_date(
+                res.raw['series'][0]['values'][0][0]).replace(tzinfo=None)
+
     return account_starts
 
 
-@click.command()
+@click.group()
+def trailts():
+    """TrailDB Time Series Index"""
+
+
+@trailts.command()
+@click.option('-c', '--config', required=True, help="Config file")
+def status(config):
+    with open(config) as fh:
+        config = yaml.safe_load(fh.read())
+    jsonschema.validate(config, CONFIG_SCHEMA)
+
+    last_index = get_incremental_starts(config, None)
+
+    accounts = {}
+    for (a, region), last in last_index.items():
+        accounts.setdefault(a, {})[region] = last
+    print yaml.safe_dump(accounts, default_flow_style=False)
+
+
+@trailts.command()
 @click.option('-c', '--config', required=True, help="Config file")
 @click.option('--start', required=True, help="Start date")
-@click.option('--end', required=True, help="End Date")
-@click.option('--incremental', default=False,
+@click.option('--end', required=False, help="End Date")
+@click.option('--incremental/--no-incremental', default=False,
               help="Sync from last indexed timestamp")
 @click.option('--concurrency', default=5)
-def index(config, start, end, incremental=False, concurrency=5):
+@click.option('-a', '--accounts', multiple=True)
+@click.option('--verbose/--no-verbose', default=False)
+def index(config, start, end, incremental=False, concurrency=5, accounts=None,
+          verbose=False):
     """index traildbs directly from s3 for multiple accounts.
 
     context: assumes a daily traildb file in s3 with key path
@@ -283,26 +313,34 @@ def index(config, start, end, incremental=False, concurrency=5):
         config = yaml.safe_load(fh.read())
     jsonschema.validate(config, CONFIG_SCHEMA)
 
+    if verbose:
+        logging.root.setLevel(logging.DEBUG)
+
     with ProcessPoolExecutor(max_workers=concurrency) as w:
         futures = {}
 
         if incremental:
-            account_starts = get_incremental_starts(config['accounts'])
+            account_starts = get_incremental_starts(config, start)
         else:
             account_starts = defaultdict(lambda : start)
 
         for account in config.get('accounts'):
-            for d in get_date_range(account_starts[account['name']], end):
-                for region in account.get('regions'):
+            if accounts and account['name'] not in accounts:
+                continue
+            for region in account.get('regions'):
+                for d in get_date_range(
+                        account_starts[(account['name'], region)], end):
                     i = bool(incremental and (d.hour or d.minute))
                     p = (config, region, account, d, i)
                     futures[w.submit(index_account, *p)] = p
 
         for f in as_completed(futures):
-            _, region, account, d = futures[f]
-            log.info("processed account:%s region:%s day:%s",
-                     account['name'], region, d.strftime("%Y/%m/%d"))
+            _, region, account, d, incremental = futures[f]
 
+            result = f.result()
+            log.info(
+                ("processed account:%(account)s day:%(day)s region:%(region)s "
+                 "records:%(records)s time:%(time)0.2f") % result)
 
 if __name__ == '__main__':
-    index(auto_envvar_prefix='TRAIL')
+    trailts(auto_envvar_prefix='TRAIL')
