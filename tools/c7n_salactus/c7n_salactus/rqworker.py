d7cc7c33e3d632335292d15291d6331979249619
Kapil Thangavelu <kapil.thangavelu@capitalone.com>
2017-03-14 08:38:02 -0400

    add doc string re why on worker

--- tools/c7n_salactus/c7n_salactus/rqworker.py
+++ tools/c7n_salactus/c7n_salactus/rqworker.py
@@ -3,6 +3,10 @@ from rq.worker import Worker
 
 class SalactusWorker(Worker):
     """Get rid of process boundary, maintain worker status.
+
+    We rely on supervisord for process supervision, and we want
+    to be able to cache sts sessions per process to avoid role
+    assume storms.
     """
 
     def execute_job(self, job, queue):
