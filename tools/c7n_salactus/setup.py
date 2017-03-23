bb31c55ab609e2ac803fbee4d98ac9c97bc07e05
Kapil Thangavelu <kapil.thangavelu@capitalone.com>
2017-03-23 13:37:58 -0400

    dial back concurrency due to load from ssl overhead

--- tools/c7n_salactus/setup.py
+++ tools/c7n_salactus/setup.py
@@ -16,7 +16,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="c7n_salactus",
-    version='0.1.7',
+    version='0.1.8',
     description="Cloud Custodian - Salactus S3",
     classifiers=[
       "Topic :: System :: Systems Administration",
