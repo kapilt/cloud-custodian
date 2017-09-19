f8fccad469094288e8e5710842fceda2e6f5622b
Kapil Thangavelu <kapil.thangavelu@capitalone.com>
2017-09-19 18:04:12 -0400

    if not using kms, then kms is sufficient

--- c7n/resources/s3.py
+++ c7n/resources/s3.py
@@ -1496,7 +1496,9 @@ class EncryptExtantKeys(ScanBucket):
 
         # If the data is already encrypted with KMS and the same key is provided
         # then we don't need to do anything
-        if info.get('ServerSideEncryption') == 'aws:kms' and self.kms_id:
+        if info.get('ServerSideEncryption') == 'aws:kms':
+            if not self.kms_id:
+                return False
             # Test using `in` because SSEKMSKeyId is the full ARN
             if self.kms_id in info.get('SSEKMSKeyId', ''):
                 return False
