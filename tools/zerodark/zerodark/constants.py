3a07a281c2d58a7748717c98be403a9b2d8b15cd
Kapil Thangavelu <kapil.thangavelu@capitalone.com>
2018-03-16 14:52:40 +0000

    some streaming

---
+++ tools/zerodark/zerodark/constants.py
@@ -0,0 +1,58 @@
+# Copyright 2017-2018 Capital One Services, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+RESOURCE_KEY = {
+    'ec2': 0,
+    'alb': 1,
+    'elb': 2,
+    'nlb': 3,
+    'emr': 4,
+    'dir': 5,
+    'lambda': 6,
+    'rds': 7,
+    'redshift': 8,
+    'elasticache': 9,
+    'nat': 10,
+    'efs-mount': 11,
+    'hsm': 12,
+    'hsmv2': 13,
+    'dms': 14,
+    'dax': 15,
+    'ecs': 16,
+    'vpce': 17,
+    'lambda': 18,
+}
+
+
+REGION_KEY = {
+    'us-east-1': 0,
+    'us-east-2': 1,
+    'us-west-1': 2,
+    'us-west-2': 3,
+    'eu-central-1': 4,
+    'eu-west-1': 5,
+    'eu-west-2': 6,
+    'eu-west-3': 7,
+    'sa-east-1': 8,
+    'ap-northeast-1': 9,
+    'ap-northeast-2': 10,
+    'ap-northeast-3': 11,
+    'ap-south-1': 12,
+    'ap-southeast-1': 13,
+    'ap-southeast-2': 14,
+    'ca-central-1': 15,
+    'cn-north-1': 16,
+    'cn-northwest-1': 17
+}
