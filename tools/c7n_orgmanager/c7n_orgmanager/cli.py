3cb840421b809ffffa54c36ca57a0a4c3df928d8
Kapil Thangavelu <kapilt@gmail.com>
2018-08-21 06:45:04 -0400

    cli parse common

--- tools/c7n_orgmanager/c7n_orgmanager/cli.py
+++ tools/c7n_orgmanager/c7n_orgmanager/cli.py
@@ -6,17 +6,34 @@ import click
 def cli():
     """multi account organization managemnet"""
 
+
+def common(f):
+    f = click.option(
+        '-a', '--accounts', multiple=True, default=None)(f)
+    f = click.option(
+        '-t', '--tags', multiple=True, default=None, help="Account tag filter")(f)
+    f = click.option(
+        '-r', '--region', default=None, multiple=True)(f)
+    return f
     
 
 @cli.command()
+@common
 def status():
     """report on multi account status"""
     
 
 @cli.command()
+@common
 def enable():
     """enable feature on org accounts."""
-    
+
+
+@cli.command()
+@common
+def disable():
+    """disable feature on org accounts."""
+
 
 if __name__ == '__main__':
     cli()
