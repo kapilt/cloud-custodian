c523e6ebddf20b7e07d0ff599d961fe2486e25c0
Kapil Thangavelu <kapilt@gmail.com>
2018-10-08 22:03:33 -0400

    iam filter binding expansion

--- tools/c7n_gcp/c7n_gcp/filters/iam.py
+++ tools/c7n_gcp/c7n_gcp/filters/iam.py
@@ -19,16 +19,50 @@ from .method import MethodFilter
 
 
 class IamAccess(MethodFilter):
-    """
+    """Filter resources by their iam policy.
+
+    users:
+      - allowed-domain
+    service-account:
+      - allowed-projects
+      - allowed-project-ids
+      - allowed-roles
     """
 
     schema = type_schema(
         'iam-policy',
+        role={'type': 'string'},
+        member={'type': 'string'},
+        member_type={'enum': ['serviceAccount', 'user', 'group', 'domain']},
         scope={'enum': ['binding', 'member']})
 
     method_spec = {
         'op': 'getIamPolicy',
         'annotation_key': 'c7n:iam-policy'}
 
+    def process_resource(self, r):
+        scope = self.data.get('scope', 'binding')
+        annotation_key = self.method_spec.get('annotation_key')
+
+        bindings = r[annotation_key]['bindings']
+
+        if scope == 'binding':
+            entities = list(bindings.values())
+        elif scope == 'member':
+            entities = []
+            for binding in bindings.values():
+                for member in binding['members']:
+                    entities.append(dict(member=member, role=binding['role']))
+        self.value_filter.annotate = False
+        matched = []
+        for e in entities:
+            matched.append(self.value_filter(e))
+
+        if matched:
+            r.setdefault('c7n:iam-matched-%s' % scope, []).extend(matched)
+        return bool(matched)
+
     def get_resource_params(self, m, r):
-        pass
+        # subclass override
+        raise NotImplementedError(
+            "iam-filter requires resource implementation of get_resource_params")
