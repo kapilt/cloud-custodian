e221a59bcbf576b5fcd27dd63b781f0ffcfbd23e
Kapil Thangavelu <kapilt@gmail.com>
2019-06-18 14:53:36 -0400

    default to loading only known cloud providers

--- c7n/resources/__init__.py
+++ c7n/resources/__init__.py
@@ -17,7 +17,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import time
-
+import os
 
 LOADED = False
 
@@ -96,11 +96,33 @@ def load_resources():
     import c7n.resources.vpc
     import c7n.resources.waf
     import c7n.resources.fsx
-    import c7n.resources.workspaces
+    import c7n.resources.workspaces  # NOQA
 
     # Load external plugins (private sdks etc)
+    #
+    # We default to loading known cloud providers
+    # to avoid the runtime costs in serverless
+    # environments of scanning the entire python
+    # path for entry points.
     from c7n.manager import resources
-    resources.load_plugins()
+    if 'C7N_EXTPLUGINS' in os.environ:
+        resources.load_plugins()
+    else:
+        try:
+            import c7n_azure.entry  # NOQA
+        except ImportError:
+            pass
+
+        try:
+            import c7n_gcp.entry  # NOQA
+        except ImportError:
+            pass
+
+        try:
+            import c7n_kube.entry  # NOQA
+        except ImportError:
+            pass
+
     resources.notify(resources.EVENT_FINAL)
 
     LOADED = True
