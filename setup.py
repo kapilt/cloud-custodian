be69a2a8479a2194b3b16eea009f4a62146667a3
Kapil Thangavelu <kapilt@gmail.com>
2020-01-27 07:05:28 -0500

    make explicit in setup metadata wrt to python versions supported

--- setup.py
+++ setup.py
@@ -15,11 +15,17 @@ setup(
     long_description_content_type='text/markdown',
     classifiers=[
         "Topic :: System :: Systems Administration",
-        "Topic :: System :: Distributed Computing"
+        "Topic :: System :: Distributed Computing",
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     url="https://github.com/cloud-custodian/cloud-custodian",
     license="Apache-2.0",
     packages=find_packages(),
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3, !=3.4, !=3.5, <4'",
     entry_points={
         'console_scripts': [
             'custodian = c7n.cli:main']},
