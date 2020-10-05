cac97349323bcdac023d4ac80fb63d174f0bcf28
Kapil Thangavelu <kapilt@gmail.com>
2020-10-05 15:27:24 -0400

    elasticip describe source subclass

--- c7n/resources/vpc.py
+++ c7n/resources/vpc.py
@@ -1956,6 +1956,12 @@ class AclAwsS3Cidrs(Filter):
         return results
 
 
+class DescribeElasticIp(query.DescribeSource):
+
+    def augment(self, resources):
+        return [r for r in resources if self.manager.resource_type.id in r]
+
+
 @resources.register('elastic-ip', aliases=('network-addr',))
 class NetworkAddress(query.QueryResourceManager):
 
@@ -1969,9 +1975,10 @@ class NetworkAddress(query.QueryResourceManager):
         filter_type = 'list'
         config_type = "AWS::EC2::EIP"
 
-    def augment(self, resources):
-        return [r for r in resources if self.resource_type.id in r]
-
+    source_mapping = {
+        'describe': DescribeElasticIp,
+        'config': query.ConfigSource
+    }
 
 NetworkAddress.filter_registry.register('shield-enabled', IsShieldProtected)
 NetworkAddress.action_registry.register('set-shield', SetShieldProtection)
