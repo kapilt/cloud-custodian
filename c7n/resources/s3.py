1c8445c6cfaf19b3a8ba362f74d789ee0062c9ea
Kapil Thangavelu <kapilt@gmail.com>
2020-10-06 09:19:39 -0400

    flake8 clean

--- c7n/resources/s3.py
+++ c7n/resources/s3.py
@@ -42,7 +42,6 @@ from botocore.exceptions import ClientError
 
 from collections import defaultdict
 from concurrent.futures import as_completed
-from dateutil.parser import parse as parse_date
 
 try:
     from urllib3.exceptions import SSLError
