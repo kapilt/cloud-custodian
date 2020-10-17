66d8403f8a08779dbe368ed467f22417c4b23828
Kapil Thangavelu <kapilt@gmail.com>
2020-10-17 07:03:31 -0400

    fix arn

--- c7n/resources/s3control.py
+++ c7n/resources/s3control.py
@@ -1,16 +1,4 @@
-# © Cloud Custodian Project and Authors. or its affiliates. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+
 from c7n.actions import Action
 from c7n.filters.iamaccess import CrossAccountAccessFilter
 from c7n.manager import resources
@@ -47,8 +35,10 @@ class S3AccessPoint(QueryResourceManager):
             'list_access_points', 'AccessPointList', None)
         filter_name = 'PolicyIds'
         filter_type = 'list'
-        arn = False
-        cfn_type = 'AWS::DLM::LifecyclePolicy'
+        arn = 'AccessPointArn'
+        arn_service =  's3'
+        arn_type = 'accesspoint'
+        cfn_type = 'AWS::S3::AccessPoint'
 
     def get_source(self, source_type):
         return {'describe': AccessPointDescribe}.get(
