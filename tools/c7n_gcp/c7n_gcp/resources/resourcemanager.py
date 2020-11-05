948a27a35b6aecdca23cf156623b9c5635759c25
Kapil Thangavelu <kapilt@gmail.com>
2020-11-05 11:24:45 -0500

    flake8 clean

--- tools/c7n_gcp/c7n_gcp/resources/resourcemanager.py
+++ tools/c7n_gcp/c7n_gcp/resources/resourcemanager.py
@@ -8,7 +8,6 @@ from c7n_gcp.query import QueryResourceManager, TypeInfo
 from c7n.utils import type_schema
 
 
-
 @resources.register('organization')
 class Organization(QueryResourceManager):
     """GCP resource: https://cloud.google.com/resource-manager/reference/rest/v1/organizations
